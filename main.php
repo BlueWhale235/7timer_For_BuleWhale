778f1064a2a5155d6d678560ecf92f54bb7e1263
bulewhale235 <2353920409@qq.com>
2022-06-29 20:42:30 +0800

    first

---
+++ main.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * This file version is [dev]! [DEV] means that this file may be change in the future.
+ * This project is personal. All data come from 7timer.org
+ * Github:BuleWhale/7timer_For_BuleWhale
+ **/
